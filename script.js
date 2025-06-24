6b7d7cb7c5225ddf16b866e1d21d469a0d56ab5f
FernandaMaressa <fernandamaressa04@gmail.com>
2025-06-24 13:51:34 -0300

    função para gerar cartas com comportamento de clique e add a lógica de clique na carta: se já tiver 2 cartas viradas, desvira, se não, vira a carta e verifica se formou um par.

--- script.js
+++ script.js
@@ -21,3 +21,34 @@ let matched = 0;
 function shuffle(array) {
   return array.sort(() => 0.5 - Math.random());
 }
+
+function createCard(src) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = url('${src}');
+
+  card.appendChild(front);
+  card.appendChild(back);
+
+  card.addEventListener('click', () => {
+    if (card.classList.contains('flip') || card.classList.contains('matched')) return;
+
+    if (flippedCards.length === 2) {
+      flippedCards.forEach(c => c.classList.remove('flip'));
+      flippedCards = [];
+    }
+
+    card.classList.add('flip');
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) checkMatch();
+  });
+
+  return card;
+}
